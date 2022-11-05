44ccb2bd4c3799a93201ac60123e82f9ea6bc6ae
stefan <noneed>
2022-11-04 23:08:58 -0500

    params progress

--- src/app/components/form-elements/date-range-picker/date-range-picker.component.ts
+++ src/app/components/form-elements/date-range-picker/date-range-picker.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-date-range-picker',
   templateUrl: './date-range-picker.component.html',
   styleUrls: ['./date-range-picker.component.scss']
 })
-export class DateRangePicker implements OnInit {
 
+export class DateRangePicker implements OnInit {
+    events: string[] = [];
+
+    range = new FormGroup({
+        start: new FormControl(null),
+        end: new FormControl(null),
+    });
   constructor() { }
 
   ngOnInit(): void {
